2c1bf03a3f977b69c141946e0340a07ed7056981
Lars Refsgaard <1552887+lkbr1808@users.noreply.github.com>
2022-09-20 13:20:23 +0200

    Change some ordering

--- install.sh
+++ install.sh
@@ -17,11 +17,12 @@ brew bundle
 # Make FISH the default shell environment
 fish fish.fish
 
-# Set macOS preferences
-# We will run this last because this will reload the shell
-source .macos
-
 
 # Symlink Mackup.cfg into the home directory
 ln Mackup.cfg ~/.mackup.cfg
 mackup restore
+
+
+# Set macOS preferences
+# We will run this last because this will reload the shell
+source .macos
