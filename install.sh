f6ec885bbef62ccedd2ddd892bef426ab4fe4592
Lars Refsgaard <1552887+LarsRefsgaard@users.noreply.github.com>
2023-09-04 13:22:21 +0200

    Evaluate brew shellenv when installing
    Also install flutter

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ echo "Setting up your Mac..."
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 # Update Homebrew recipes
--- work.sh
+++ work.sh
@@ -2,4 +2,7 @@ read -e -p "Is this a work machine? " choice
 if [[ "$choice" == [Yy]* ]]; then
     echo "Installing work packages"
     brew bundle --file Brewfile.work
+
+    git clone https://github.com/flutter/flutter.git -b stable
+    set -Ua fish_user_paths ~/flutter/bin/
 fi
